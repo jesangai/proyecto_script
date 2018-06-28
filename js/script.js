965e451d9d95a3ac7263bcc35e8e905eddfa916b
jesangai <jagg1996.jj@gmail.com>
2018-06-28 12:48:32 -0500

    js avance

--- js/script.js
+++ js/script.js
@@ -32,3 +32,71 @@ class Actividad{
         this.imagen4=imagen4;
     }
 }
+$('#video').change(function () {
+    //obtenemos un array con los datos del archivo
+    var file = $("#video")[0].files[0];
+    //obtenemos el nombre del archivo
+    var fileName = file.name;
+    //obtenemos la extensión del archivo
+    var fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+    //obtenemos el tamaño del archivo
+    var fileSize = file.size;
+    //obtenemos el tipo de archivo image/png ejemplo
+    var fileType = file.type;
+    //mensaje con la información del archivo
+    //showMessageE("<span>Archivo para subir: " + fileName + ", peso total: " + fileSize + " bytes.</span>");
+});
+
+$('.añadirvideo').click(function () {
+    //información del formulario
+    var formData = new FormData($(".formularioVideo")[0]);
+    var message = "";
+    alert(formData);
+    //hacemos la petición ajax  
+    $.ajax({
+        url: 'subir.php',
+        type: 'POST',
+        // Form data
+        //datos del formulario
+        data: formData,
+        //necesario para subir archivos via ajax
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            // message = $("<span>La imagen ha subido correctamente.</span>");
+            //showMessageE(message);
+            alert(data);
+            if (isImage(fileExtension)) {
+                $(".escenas").html("<p>miau</p>")}
+        },
+        //si ha ocurrido un error
+        error: function () {
+            alert("error");
+            // message = $("<span>Ha ocurrido un error.</span>");
+            //showMessageE(message);<video><source src='../excursiones/videos/'" + data + "' type='video/mp4'/></video>
+        }
+    });
+});
+
+function isImage(extension) {
+    switch (extension.toLowerCase()) {
+        case 'jpg':
+        case 'gif':
+        case 'png':
+        case 'jpeg':
+            return true;
+            break;
+        case 'mp3':
+        case 'wav':
+        case 'mp4':
+        case 'ogg':
+            return true;
+            break;
+        default:
+            return false;
+            break;
+    }
+}
+
+
